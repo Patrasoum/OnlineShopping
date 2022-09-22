ed21ac44c16c564d043ee25ebbdbc9decf7aacfb
Soumen Patra <104466514+Patrasoum@users.noreply.github.com>
2022-09-22 11:32:52 +0530

    Update FeignServiceProductUtil.java

--- feigncs2/feigncs2/src/main/java/com/example/feigncs2/utill/FeignServiceProductUtil.java
+++ feigncs2/feigncs2/src/main/java/com/example/feigncs2/utill/FeignServiceProductUtil.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-@FeignClient(value = "product-service-c2",url = "http://localhost:3001/products")
+@FeignClient(value = "product-service-cs2",url = "http://localhost:3001/products")
 public interface FeignServiceProductUtil {
 
     @GetMapping("/Products")
