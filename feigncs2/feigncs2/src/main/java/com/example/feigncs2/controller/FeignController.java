d65120f584c943545ba414cb261ad89c12b30605
Soumen Patra <104466514+Patrasoum@users.noreply.github.com>
2022-09-22 11:37:23 +0530

    Update FeignController.java

--- feigncs2/feigncs2/src/main/java/com/example/feigncs2/controller/FeignController.java
+++ feigncs2/feigncs2/src/main/java/com/example/feigncs2/controller/FeignController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/feign-c2")
+@RequestMapping("/feign-cs2")
 public class FeignController {
 
     @Autowired
