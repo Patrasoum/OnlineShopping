ea1881d3d7ae065500123f5fd53b44e1570f811e
Soumen Patra <104466514+Patrasoum@users.noreply.github.com>
2022-09-22 11:30:35 +0530

    Update FeignServiceCustomerUtil.java

--- feigncs2/feigncs2/src/main/java/com/example/feigncs2/utill/FeignServiceCustomerUtil.java
+++ feigncs2/feigncs2/src/main/java/com/example/feigncs2/utill/FeignServiceCustomerUtil.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "customer-service-c2",url = "http://localhost:3002/customers")
+@FeignClient(value = "customer-service-cs2",url = "http://localhost:3002/customers")
 public interface FeignServiceCustomerUtil {
 
     @GetMapping("/Customers")
