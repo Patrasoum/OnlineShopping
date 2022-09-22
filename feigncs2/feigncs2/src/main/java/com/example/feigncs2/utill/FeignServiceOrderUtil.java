9e4347fde1539ef35d4a526cdabeb29926e72652
Soumen Patra <104466514+Patrasoum@users.noreply.github.com>
2022-09-22 11:32:10 +0530

    Update FeignServiceOrderUtil.java

--- feigncs2/feigncs2/src/main/java/com/example/feigncs2/utill/FeignServiceOrderUtil.java
+++ feigncs2/feigncs2/src/main/java/com/example/feigncs2/utill/FeignServiceOrderUtil.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "order-service-c2",url = "http://localhost:3003/orders")
+@FeignClient(value = "order-service-cs2",url = "http://localhost:3003/orders")
 public interface FeignServiceOrderUtil {
 
     @GetMapping("/Orders")
